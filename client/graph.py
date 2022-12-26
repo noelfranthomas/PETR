212a14f1f1c46feafbb12f408cc7b564572cf06c
noelfranthomas <noelfranthomas@gmail.com>
2022-12-26 13:27:00 -0700

    Update graph.py

--- client/graph.py
+++ client/graph.py
@@ -1,3 +1,26 @@
 # https://medium.com/nlplanet/building-a-knowledge-base-from-texts-a-full-practical-example-8dbbffb912fa
-# https://towardsdatascience.com/knowledge-graphs-in-natural-language-processing-acl-2021-6cac04f39761
-# https://www.youtube.com/watch?v=LfnrRPFhkuY
+# RE Methods: https://medium.com/@andreasherman/different-ways-of-doing-relation-extraction-from-text-7362b4c3169ehttps://medium.com/@andreasherman/different-ways-of-doing-relation-extraction-from-text-7362b4c3169e
+# Spacy RE: https://www.youtube.com/watch?v=8HL-Ap5_Axo
+# ACL Collection: https://towardsdatascience.com/knowledge-graphs-in-natural-language-processing-acl-2021-6cac04f39761
+# LSTM Simplified: https://www.youtube.com/watch?v=LfnrRPFhkuY
+
+
+
+
+
+# Relation Extraction
+# https://www.cs.cmu.edu/~nbach/papers/A-survey-on-Relation-Extraction.pdf
+
+# Entity Linking
+
+# https://www.youtube.com/watch?v=8u57WSXVpmw
+
+
+
+# Tokenizer: TikToken: https://github.com/openai/tiktoken
+
+# Plan:
+# https://medium.com/nlplanet/building-a-knowledge-base-from-texts-a-full-practical-example-8dbbffb912fa
+#   - Spacy RE: https://www.youtube.com/watch?v=8HL-Ap5_Axo
+#       - Tokenizer: TikToken: https://github.com/openai/tiktoken
+#   - Use REBEL Larg from HF
